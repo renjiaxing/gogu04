b3bdd60dbd42bc6d92bd2dfd3104425383784ff1
ren <ren_jiaxing@163.com>
2015-03-31 22:37:18 +0800

    Initial Commit

---
+++ gogu04/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gogu04
+//
+//  Created by ren on 15/3/31.
+//  Copyright (c) 2015年 ren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gogu04/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gogu04
+//
+//  Created by ren on 15/3/31.
+//  Copyright (c) 2015年 ren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
